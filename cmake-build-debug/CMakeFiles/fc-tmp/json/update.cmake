205aa4662eda8db2d90c947917d316b8b427c65e
whencanibe <eonyakcho@gmail.com>
2025-05-23 18:50:30 +0900

    Reinitialize tracking with .gitignore

--- cmake-build-debug/CMakeFiles/fc-tmp/json/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION 3.29)
-
-message(VERBOSE "Executing update step for json")
-
-block(SCOPE_FOR VARIABLES)
-
-include("/Users/eonyak/CLionProjects/MiniHttpServer/cmake-build-debug/CMakeFiles/fc-tmp/json/json-gitupdate.cmake")
-
-endblock()
